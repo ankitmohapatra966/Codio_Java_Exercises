de1907dcbc2c9cfe0a1713f89b7623a83091ead9
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-03-22 11:30:23 +0530

    first commit

---
+++ AdvancedLab_Exercise4.java
@@ -0,0 +1,93 @@
+public class Exercise4 {  
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+        //add code below this line
+
+    // Library library = new Library();
+    // Book book1 = new Book("Three Musketeers", "Alexandre Dumas", "fiction");
+    // Book book2 = new Book("The Count of Monte Cristo", "Alexandre Dumas", "fiction");
+    // Book book3 = new Book("Educated", "Tara Westover", "nonfiction");
+
+    // library.addBook(book1);
+    // library.addBook(book2);
+    // library.addBook(book3);
+    // library.sortBooks();
+    // System.out.println(library.searchTitle("Moby Dick"));
+
+    //add code above this line
+
+    //add code above this line
+        //add code below this line
+
+    // Library library = new Library();
+    // Book book1 = new Book("Three Musketeers", "Alexandre Dumas", "fiction");
+    // Book book2 = new Book("The Count of Monte Cristo", "Alexandre Dumas", "fiction");
+    // Book book3 = new Book("Educated", "Tara Westover", "nonfiction");
+
+    // library.addBook(book1);
+    // library.addBook(book2);
+    // library.addBook(book3);
+    // library.sortBooks();
+    // System.out.println(library.searchTitle("Educated"));
+
+    //add code above this line
+        //add code below this line
+
+    // Library library = new Library();
+    // Book book1 = new Book("Three Musketeers", "Alexandre Dumas", "fiction");
+    // Book book2 = new Book("The Count of Monte Cristo", "Alexandre Dumas", "fiction");
+    // Book book3 = new Book("Educated", "Tara Westover", "nonfiction");
+
+    // library.addBook(book1);
+    // library.addBook(book2);
+    // library.addBook(book3);
+    // library.sortBooks();
+    // System.out.println(library.searchAuthor("Herman Melville"));
+
+    //add code above this line
+        //add code below this line
+
+    // Library library = new Library();
+    // Book book1 = new Book("Three Musketeers", "Alexandre Dumas", "fiction");
+    // Book book2 = new Book("The Count of Monte Cristo", "Alexandre Dumas", "fiction");
+    // Book book3 = new Book("Educated", "Tara Westover", "nonfiction");
+
+    // library.addBook(book1);
+    // library.addBook(book2);
+    // library.addBook(book3);
+    // library.sortBooks();
+    // System.out.println(library.getNonfiction());
+
+    //add code above this line
+        //add code below this line
+
+    Library library = new Library();
+    Book book1 = new Book("Three Musketeers", "Alexandre Dumas", "fiction");
+    Book book2 = new Book("The Count of Monte Cristo", "Alexandre Dumas", "fiction");
+    Book book3 = new Book("Educated", "Tara Westover", "nonfiction");
+
+    library.addBook(book1);
+    library.addBook(book2);
+    library.addBook(book3);
+    library.sortBooks();
+    System.out.println(library.getFiction());
+
+    //add code above this line
+  }
+}
+
+//For file Book.java
+    //add code below this line
+
+    Item item1 = new Item("milk", 1.5, 1);
+    Item item2 = new Item("apple", 0.75, 5);
+    Item item3 = new Item("bread", 2.25, 2);
+    ShoppingCart cart = new ShoppingCart();
+
+    cart.addItem(item1);
+    cart.addItem(item2);
+    cart.addItem(item3);
+
+    //add code above this line
